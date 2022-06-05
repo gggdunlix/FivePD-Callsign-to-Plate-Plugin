1fda663186f07b158cc56f43023e251ba2aaa613
BuildTools <unconfigured@null.spigotmc.org>
2022-06-05 02:07:16 -0400

    2.0.0, allow per department setting

--- FivePD Callsign to Plate Plugin/PlateToCallsign.cs
+++ FivePD Callsign to Plate Plugin/PlateToCallsign.cs
@@ -5,6 +5,9 @@ using CitizenFX.Core;
 using FivePD.API;
 using FivePD.API.Utils;
 using CitizenFX.Core.Native;
+using Newtonsoft.Json.Linq;
+using System.Linq;
+using Newtonsoft.Json.Schema;
 
 namespace PlateToCallsign
 {
@@ -21,10 +24,32 @@ namespace PlateToCallsign
             {
                 if (onDuty)
                 {
-                    EventHandlers["FivePD::Client::SpawnVehicle"] += new Action<int, int>(DoPlateToCallsign); //Natixco
+                    EventHandlers["FivePD::Client::SpawnVehicle"] += new Action<int, int>(CheckDepartment); //Natixco
                 }
                 else
                 {
+                }
+            }
+            private void CheckDepartment(int playerServerID, int vehicleNetworkID) //suggested by Lucky
+            {
+                PlayerData data = Utilities.GetPlayerData();
+
+                int playerdepartment = data.DepartmentID;
+                var file = API.LoadResourceFile(API.GetCurrentResourceName(), "/config/vehicles.json");
+                JToken config = JToken.Parse(file);
+                if (config["plateToCallsignDepartments"] != null)
+                {
+                    foreach (int Department in config["plateToCallsignDepartments"])
+                    {
+                        if (playerdepartment == Department)
+                        {
+                            DoPlateToCallsign(LocalPlayer.ServerId, Game.PlayerPed.CurrentVehicle.NetworkId);
+                        }
+                    }
+                    
+                } else
+                {
+                    DoPlateToCallsign(LocalPlayer.ServerId, Game.PlayerPed.CurrentVehicle.NetworkId);
                 }
             }
             private void DoPlateToCallsign(int playerServerId, int vehicleNetworkId)
@@ -33,13 +58,15 @@ namespace PlateToCallsign
                 {
 
                     PlayerData data = Utilities.GetPlayerData();
-                    int ActualVehicleID = Game.PlayerPed.CurrentVehicle.NetworkId;
-                    string Callsign = data.Callsign;
-                    
-                    //with help from Grandpa Rex
 
-                    var vehicle = Entity.FromNetworkId(vehicleNetworkId);
-                    API.SetVehicleNumberPlateText(vehicle.Handle, Callsign);
+                        int ActualVehicleID = Game.PlayerPed.CurrentVehicle.NetworkId;
+                        string Callsign = data.Callsign;
+
+                        //with help from Grandpa Rex
+
+                        var vehicle = Entity.FromNetworkId(vehicleNetworkId);
+                        API.SetVehicleNumberPlateText(vehicle.Handle, Callsign);
+                    
                 }
 
             }
